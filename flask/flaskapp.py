4a22b2a1ab172a1124196f57cea56b3a62c1ab32
Noohu Nufais Sulaiman <noohunufais.s@gmail.com>
2024-09-30 21:53:04 -0600

    huge change

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -1,9 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route("/add_device", methods=['GET','POST'])
+def add_device():
+    if request.method == 'GET':
+        return render_template('add_device.html')
+    else:
+        return request.form
+
+
+@app.route("/read_form", methods=['POST'])
+def read_form():
+    return request.form
+        
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
---
+++ script.py
@@ -0,0 +1,21 @@
+from jinja2 import Template
+import json
+
+template_file = "/home/student/iac_network/templates/R.j2"
+
+variable_file = "/home/student/iac_network/variables/R1.json"
+
+with open(variable_file) as json_file:
+    config_items = json.load(json_file)
+
+with open(template_file) as f:
+    cisco_template = Template(f.read(), keep_trailing_newline=True)
+
+startup_config = cisco_template.render(config_items)
+
+
+
+golden_config_file_path = '/home/student/iac_network/golden_configs/R1.cfg'  
+
+with open(golden_config_file_path, 'w') as output_file:
+    output_file.write(startup_config)
