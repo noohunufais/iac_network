3f714fc15e37fa677140b10cb9cfe4e438210b70
Noohu Nufais Sulaiman <noohunufais.s@gmail.com>
2024-11-04 17:01:20 -0700

    changes

--- flask/flaskapp.py
+++ flask/flaskapp.py
@@ -47,16 +47,16 @@ def config_push():
         device_name = request.form['deviceName']
 
         # Capture VLAN data
-        vlan_numbers = request.form.getlist('vlan[]')
-        vlan_names = request.form.getlist('vlan_name[]')
+        # vlan_numbers = request.form.getlist('vlan[]')
+        # vlan_names = request.form.getlist('vlan_name[]')
 
         # Capture Interface data
-        interface_names = request.form.getlist('interface_name[]')  # List of Interface names
-        ipv4_addresses = request.form.getlist('ipv4_address[]')  # List of IPv4 addresses
-        ipv4_subnets = request.form.getlist('ipv4_subnet[]')  # List of IPv4 subnets
-        ipv6_addresses = request.form.getlist('ipv6_address[]')  # List of IPv6 addresses
-        ipv6_prefixes = request.form.getlist('ipv6_prefix[]')  # List of IPv6 prefixes
-        switchports = request.form.getlist('switchport[]')
+        # interface_names = request.form.getlist('interface_name[]') 
+        # ipv4_addresses = request.form.getlist('ipv4_address[]') 
+        # ipv4_subnets = request.form.getlist('ipv4_subnet[]') 
+        # ipv6_addresses = request.form.getlist('ipv6_address[]') 
+        # ipv6_prefixes = request.form.getlist('ipv6_prefix[]')  
+        # switchports = request.form.getlist('switchport[]')
 
         # Capture IPv4 Static Route data
         ipv4_dest_networks = request.form.getlist('ipv4_dest_network[]')
@@ -64,73 +64,73 @@ def config_push():
         ipv4_next_hops = request.form.getlist('ipv4_next_hop[]')
 
         # Capture IPv6 Static Route data
-        ipv6_dest_networks = request.form.getlist('ipv6_dest_network[]')
-        ipv6_prefixes = request.form.getlist('ipv6_prefix[]')
-        ipv6_next_hops = request.form.getlist('ipv6_next_hop[]')
+        # ipv6_dest_networks = request.form.getlist('ipv6_dest_network[]')
+        # ipv6_prefixes = request.form.getlist('ipv6_prefix[]')
+        # ipv6_next_hops = request.form.getlist('ipv6_next_hop[]')
 
         # Capture Logging data
-        log_trap_type = request.form.get('log_trap_type')
-        log_host_ip = request.form.get('log_host_ip')
-        log_source_address = request.form.get('log_source_address')
+        # log_trap_type = request.form.get('log_trap_type')
+        # log_host_ip = request.form.get('log_host_ip')
+        # log_source_address = request.form.get('log_source_address')
 
         # Capture SNMP data
-        snmp_version = request.form.get('snmp_version')
-        community_string = request.form.get('community_string')
-        snmp_permission = request.form.get('snmp_permission')
-        snmp_host_ip = request.form.get('snmp_host_ip')
+        # snmp_version = request.form.get('snmp_version')
+        # community_string = request.form.get('community_string')
+        # snmp_permission = request.form.get('snmp_permission')
+        # snmp_host_ip = request.form.get('snmp_host_ip')
 
         # Capture OSPFv2 Network data
-        ospfv2_networks = request.form.getlist('ospfv2_network[]')
-        ospfv2_subnet_masks = request.form.getlist('ospfv2_subnet_mask[]')
-        ospfv2_areas = request.form.getlist('ospfv2_area[]')
+        # ospfv2_networks = request.form.getlist('ospfv2_network[]')
+        # ospfv2_subnet_masks = request.form.getlist('ospfv2_subnet_mask[]')
+        # ospfv2_areas = request.form.getlist('ospfv2_area[]')
 
         # Capture Redistribute data
-        ospfv2_redistribute_bgp = request.form.get('ospfv2_redistribute_bgp')
-        ospfv2_redistribute_rip = request.form.get('ospfv2_redistribute_rip')
-        ospfv2_redistribute_static = request.form.get('ospfv2_redistribute_static')
-        ospfv2_redistribute_connected = request.form.get('ospfv2_redistribute_connected')
+        # ospfv2_redistribute_bgp = request.form.get('ospfv2_redistribute_bgp')
+        # ospfv2_redistribute_rip = request.form.get('ospfv2_redistribute_rip')
+        # ospfv2_redistribute_static = request.form.get('ospfv2_redistribute_static')
+        # ospfv2_redistribute_connected = request.form.get('ospfv2_redistribute_connected')
 
         # Debugging: Print the captured data to the console (for testing purposes)
-        print("VLAN Data:")
-        for i, (vlan_num, vlan_name) in enumerate(zip(vlan_numbers, vlan_names), start=1):
-            print(f"VLAN {i}: Number={vlan_num}, Name={vlan_name}")
+        # print("VLAN Data:")
+        # for i, (vlan_num, vlan_name) in enumerate(zip(vlan_numbers, vlan_names), start=1):
+        #     print(f"VLAN {i}: Number={vlan_num}, Name={vlan_name}")
 
-        print("\nInterface Data:")
-        for i, (iface_name, ipv4, subnet, ipv6, prefix, switchport) in enumerate(zip(interface_names, ipv4_addresses, ipv4_subnets, ipv6_addresses, ipv6_prefixes, switchports), start=1):
-            print(f"Interface {i}: Name={iface_name}, IPv4={ipv4}, Subnet={subnet}, IPv6={ipv6}, Prefix={prefix}, Switchport={switchport}")
+        # print("\nInterface Data:")
+        # for i, (iface_name, ipv4, subnet, ipv6, prefix, switchport) in enumerate(zip(interface_names, ipv4_addresses, ipv4_subnets, ipv6_addresses, ipv6_prefixes, switchports), start=1):
+        #     print(f"Interface {i}: Name={iface_name}, IPv4={ipv4}, Subnet={subnet}, IPv6={ipv6}, Prefix={prefix}, Switchport={switchport}")
 
         print("\nIPv4 Static Route Data:")
         for i, (dest, subnet, next_hop) in enumerate(zip(ipv4_dest_networks, ipv4_subnets, ipv4_next_hops), start=1):
             print(f"Route {i}: Destination Network={dest}, Subnet Mask={subnet}, Next Hop={next_hop}")
 
-        print("\nIPv6 Static Route Data:")
-        for i, (dest, prefix, next_hop) in enumerate(zip(ipv6_dest_networks, ipv6_prefixes, ipv6_next_hops), start=1):
-            print(f"Route {i}: Destination Network={dest}, Prefix={prefix}, Next Hop={next_hop}")
+        # print("\nIPv6 Static Route Data:")
+        # for i, (dest, prefix, next_hop) in enumerate(zip(ipv6_dest_networks, ipv6_prefixes, ipv6_next_hops), start=1):
+        #     print(f"Route {i}: Destination Network={dest}, Prefix={prefix}, Next Hop={next_hop}")
 
-        print("\nLogging Data:")
-        print(f"Trap Type: {log_trap_type}")
-        print(f"Host IP: {log_host_ip}")
-        print(f"Source Address: {log_source_address}")
+        # print("\nLogging Data:")
+        # print(f"Trap Type: {log_trap_type}")
+        # print(f"Host IP: {log_host_ip}")
+        # print(f"Source Address: {log_source_address}")
 
-        print("SNMP Data:")
-        print(f"SNMP Version: {snmp_version}")
-        print(f"Community String: {community_string}")
-        print(f"Permission: {snmp_permission}")
-        print(f"Host IP: {snmp_host_ip}")
+        # print("SNMP Data:")
+        # print(f"SNMP Version: {snmp_version}")
+        # print(f"Community String: {community_string}")
+        # print(f"Permission: {snmp_permission}")
+        # print(f"Host IP: {snmp_host_ip}")
 
-        print("Basic Settings Data:")
-        print(f"Hostname: {hostname}")
-        print(f"Username: {username}")
-        print(f"Password: {password}")
-        print(f"Privilege: {privilege}")
-        print(f"Role: {role}")
+        # print("Basic Settings Data:")
+        # print(f"Hostname: {hostname}")
+        # print(f"Username: {username}")
+        # print(f"Password: {password}")
+        # print(f"Privilege: {privilege}")
+        # print(f"Role: {role}")
 
-        print("OSPFv2 Network Data:")
-        for i, (network, subnet, area) in enumerate(zip(ospfv2_networks, ospfv2_subnet_masks, ospfv2_areas), start=1):
-            print(f"Network {i}: Network={network}, Subnet Mask={subnet}, Area Number={area}")
+        # print("OSPFv2 Network Data:")
+        # for i, (network, subnet, area) in enumerate(zip(ospfv2_networks, ospfv2_subnet_masks, ospfv2_areas), start=1):
+        #     print(f"Network {i}: Network={network}, Subnet Mask={subnet}, Area Number={area}")
 
-        print("\nRedistribute Options:")
-        print(f"BGP: {ospfv2_redistribute_bgp}, RIP: {ospfv2_redistribute_rip}, Static: {ospfv2_redistribute_static}, Connected: {ospfv2_redistribute_connected}")
+        # print("\nRedistribute Options:")
+        # print(f"BGP: {ospfv2_redistribute_bgp}, RIP: {ospfv2_redistribute_rip}, Static: {ospfv2_redistribute_static}, Connected: {ospfv2_redistribute_connected}")
 
         return request.form
     else:
--- test.py
+++ test.py
@@ -1,43 +1,16 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-# Load YAML data from variables/routing_config.yaml
-with open("variables/config.yml", "r") as yaml_file:
-    config_data = yaml.safe_load(yaml_file)
+# Load YAML configuration
+with open('variables/config.yaml') as file:
+    config_data = yaml.safe_load(file)
 
-# Set up the Jinja environment to load templates from the templates directory
-env = Environment(loader=FileSystemLoader("templates"))
+# Setup Jinja2 environment
+env = Environment(loader=FileSystemLoader('templates'))
 
-# Load both IPv4 and IPv6 templates
-ipv4_template = env.get_template("ipv4_static_route.j2")
-ipv6_template = env.get_template("ipv6_static_route.j2")
-vlan_template = env.get_template("vlan.j2")
-interface_template = env.get_template("interface.j2")
-ospf_template = env.get_template('ospfv2.j2')
-snmp_template = env.get_template('snmp.j2')
-logging_template = env.get_template('logging.j2')
+# Render main template
+main_template = env.get_template('main.j2')
+config_output = main_template.render(config_data)
 
-# Render both templates using the respective data
-ipv4_output = ipv4_template.render(ipv4_routes=config_data["ipv4_routes"])
-ipv6_output = ipv6_template.render(ipv6_routes=config_data["ipv6_routes"])
-vlan_output = vlan_template.render(vlans=config_data["vlans"])
-interface_output = interface_template.render(interfaces=config_data["interfaces"])
-ospf_config = ospf_template.render(ospf=config_data['ospf'])
-snmp_config = snmp_template.render(snmp=config_data['snmp'])
-logging_config = logging_template.render(logging=config_data['logging'])
-
-# Print the outputs with clear section headings
-print("IPv4 Routes Configuration:")
-print(ipv4_output)
-print("\nIPv6 Routes Configuration:")
-print(ipv6_output)
-print("\nVLAN Configuration:")
-print(vlan_output)
-print("\nInterface Configuration:")
-print(interface_output)
-print("\nOSPF Configuration:")
-print(ospf_config)
-print("\nSNMP Configuration:")
-print(snmp_config)
-print("\nLogging Configuration:")
-print(logging_config)
+# Output configuration
+print(config_output)
